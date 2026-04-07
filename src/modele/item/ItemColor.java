d8837fb8f1e04fb52a42423e07fdc61c90ace0e6
David Chafizadeh <david.chafizadeh@etu.univ-lyon1.fr>
2026-04-07 11:40:13 +0200

    Modif itemColor

--- src/modele/item/ItemColor.java
+++ src/modele/item/ItemColor.java
@@ -2,9 +2,11 @@ package modele.item;
 
 public class ItemColor extends Item {
     Color color;
-
+    public Color getColor(){
+        return color;
+    }
     public void transform(Color add) { // faire varier la couleur suivant la couleur ajoutée
-
+        color=add;
     }
 
 }
